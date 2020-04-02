fb3feb1a7878b032c3edd917c1f55f7da4c3488d
gh0stwizard <vitaliy.tokarev@gmail.com>
2020-04-02 17:18:28 +0300

    added wget support to misc/update-iana-db.sh

--- misc/update-iana-db.sh
+++ misc/update-iana-db.sh
@@ -9,29 +9,52 @@ CURDIR=`dirname $0`
 SRCDIR=$(realpath "${CURDIR}/../")
 # customize this if built with cmake
 APP=$(realpath ${SRCDIR}/iana-tld-extractor)
-HTML_FILE=last.html
+#APP=$(realpath ${SRCDIR}/build/iana-tld-extractor)
+HTML_FILE="last.html"
+IANA_URL="https://www.iana.org/domains/root/db"
 LOG="logger -s update-iana-db.sh: "
+CURL=`which curl`
+WGET=`which wget`
 
 cd "${SRCDIR}" || exit 1
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:myhtml/lib
 
-git pull || exit 1
 
-curl -s -o ${HTML_FILE} -L https://www.iana.org/domains/root/db || exit 1
+download() {
+	if [ -n "${CURL}" ]; then
+		${CURL} -s -o ${HTML_FILE} -L ${IANA_URL} || exit 1
+	elif [ -n "${WGET}" ]; then
+		${WGET} -q -O ${HTML_FILE} ${IANA_URL} || exit 1
+	else
+		$LOG "ERROR: neither curl or wget has installed."
+		exit 1
+	fi
+}
+
 
 if [ -x ${APP} ]; then
-	./build/iana-tld-extractor ${HTML_FILE} > tld.csv
-	./build/iana-tld-extractor -r ${HTML_FILE} > raw.csv
-elif [ -x iana-tld-extractor ]; then
-	./iana-tld-extractor ${HTML_FILE} > tld.csv
-	./iana-tld-extractor -r ${HTML_FILE} > raw.csv
+	git pull || exit 1
+	download
+	# update CSV files
+	${APP} ${HTML_FILE} > tld.csv
+	${APP} -r ${HTML_FILE} > raw.csv
+else
+	$LOG "${APP} does not exist or is not executable"
+	exit 1
 fi
 
-git diff --exit-code && $LOG "Nothing to update" && exit 0
+git diff --exit-code tld.csv raw.csv && \
+	$LOG "Nothing to update" && \
+	exit 0
 
-$LOG "Updating IANA database ..."
-datetime=`date -u`
-git add tld.csv raw.csv
-git commit -m "update database ${datetime}"
-git push || exit 1
-exit 0
+if [ -s "tld.csv" ] && [ -s "raw.csv" ]; then
+	$LOG "Updating IANA database ..."
+	datetime=`date -u`
+	git add tld.csv raw.csv
+	git commit -m "update database ${datetime}"
+	git push || exit 1
+else
+	$LOG "ERROR: tld.csv and raw.csv files are empty!"
+	git checkout tld.csv raw.csv
+	exit 1
+fi
